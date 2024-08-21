43248441dcb88b993c43f44f5aba33700948dcd8
GOSS <goss1997@naver.com>
2024-08-21 13:31:32 +0900

    create final-project
    파이널 프로젝트 생성

---
+++ figurium/src/test/java/com/githrd/figurium/FiguriumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githrd.figurium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiguriumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

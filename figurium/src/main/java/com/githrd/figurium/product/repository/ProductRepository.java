0902e4a2dc39fec8293960679e538add07068f28
ryusuncheol <ryusuncheol@naver.com>
2024-08-28 20:15:38 +0900

    fix : ProductRepository   findById 코드 재기재
    fix : ProductRepository   findById 코드 재기재

--- figurium/src/main/java/com/githrd/figurium/product/repository/ProductRepository.java
+++ figurium/src/main/java/com/githrd/figurium/product/repository/ProductRepository.java
@@ -20,6 +20,9 @@ public interface ProductRepository extends JpaRepository<Products, Integer> {
     @Query("SELECT p FROM Products p WHERE p.id > :lastId ORDER BY p.id ASC")
         //List<Products> findByIdGreaterThanOrderByIdAsc(@Param("lastId") int lastId, Pageable pageable);
 
+    // select one
+    Products findById(int id);
+
     Page<Products> findByIdGreaterThanOrderByIdAsc(int lastId, Pageable pageable);
 
     // 선일 주문 화면에 나올 상품 List에 담길 DB

ca751a4cdf7c6593acb8d75326737ff774c3e6d7
chaSunil <cktjsdlf4636@naver.com>
2024-08-31 19:59:48 +0900

    feat(CartsController.java) : 장바구니 추가 로직 구현
    장바구니 추가 로직 구현

--- figurium/src/main/java/com/githrd/figurium/product/controller/CartsController.java
+++ figurium/src/main/java/com/githrd/figurium/product/controller/CartsController.java
@@ -1,13 +1,49 @@
 package com.githrd.figurium.product.controller;
 
+import com.githrd.figurium.product.dao.CartsMapper;
+import com.githrd.figurium.user.entity.User;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class CartsController {
 
+    private HttpSession session;
+    private CartsMapper cartsMapper;
+
+    @Autowired
+    public CartsController(HttpSession session, CartsMapper cartsMapper) {
+        this.session = session;
+        this.cartsMapper = cartsMapper;
+    }
+
+
     @GetMapping("/shopingCart.do")
-        public String shopingCart(){
+        public String shopingCart(Model model, String productId, int quantity) {
+
+        // 세션에서 로그인 사용자 정보 가져오기
+        User loginUser = (User)session.getAttribute("loginUser");
+
+        // 로그인 사용자 정보가 null인지 체크
+        if (loginUser == null) {
+            model.addAttribute("errorMessage", "로그인 후 구매가 가능합니다.");
+            return "redirect:/";
+        }
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("loginUserId", loginUser.getId());
+        map.put("productId", productId);
+        map.put("quantity", quantity);
+
+
+        // 정바구니에 해당 ID로 상품 추가
+        cartsMapper.insertCarts(map);
 
 
 
--- figurium/src/main/java/com/githrd/figurium/product/dao/CartsMapper.java
+++ figurium/src/main/java/com/githrd/figurium/product/dao/CartsMapper.java
@@ -3,8 +3,10 @@ package com.githrd.figurium.product.dao;
 import com.githrd.figurium.product.vo.CartsVo;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.Map;
+
 @Mapper
 public interface CartsMapper {
 
-    int insertCarts(CartsVo cartsVo);
+    int insertCarts(Map<String, Object> map);
 }

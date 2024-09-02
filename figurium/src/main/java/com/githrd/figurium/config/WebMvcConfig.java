803d68710d11c1c6cb98d25c15349f632e79363c
GOSS <goss1997@naver.com>
2024-09-02 14:07:44 +0900

    feat(WebMvcConfig.java): viewResolver 추가
    jsp를 사용하기 위해 viewResolver에 접두사 접미사 추가 구현.

--- figurium/src/main/java/com/githrd/figurium/config/WebMvcConfig.java
+++ figurium/src/main/java/com/githrd/figurium/config/WebMvcConfig.java
@@ -2,16 +2,27 @@ package com.githrd.figurium.config;
 
 import com.githrd.figurium.exception.MyCustomExceptionResolver;
 import com.githrd.figurium.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
 
 
     /**

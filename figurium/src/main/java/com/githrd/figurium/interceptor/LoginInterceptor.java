dac5f05386cdf36cda2b9e79f1cf627734278f0b
Goss <goss1997@naver.com>
2024-09-03 01:58:34 +0900

    fix(LoginInterceptor.java): 인터셉터 일단 주석 처리.
    jsp 인식이 안되서 요청을 중간에 가로챈 상태에서 검증 후 요청을 수행하는데
    뷰 리졸버 설정을 하였음에도 webapp 쪽 인식이 잘 안되서 다른 에러가 나도 jsp를 찾을 수 없다는 에러가 자꾸 남으로 일단 주석 처리함.

--- figurium/src/main/java/com/githrd/figurium/interceptor/LoginInterceptor.java
+++ figurium/src/main/java/com/githrd/figurium/interceptor/LoginInterceptor.java
@@ -6,8 +6,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import java.io.PrintWriter;
-
 public class LoginInterceptor implements HandlerInterceptor {
 
     private final static String LOGIN_USER = "loginUser";
@@ -18,15 +16,31 @@ public class LoginInterceptor implements HandlerInterceptor {
         HttpSession session = request.getSession();
         User loginUser = (User) session.getAttribute(LOGIN_USER);
 
-        // session에 loginUser가 없을 경우
-        if (loginUser == null) {
-            // 클라이언트에 alert 창을 띄우고, 메인 페이지로 리다이렉트
-            response.setContentType("text/html; charset=UTF-8");
-            PrintWriter out = response.getWriter();
-            out.println("<script>alert('로그인이 필요한 페이지입니다.'); location.href='/';</script>");
-            out.flush();    // 응답이 바로 전송되도록 버퍼 비우기
-            return false;  // 요청 처리를 중단
-        }
+        // ajax 요청인지 확인.
+        boolean isAjaxRequest = "XMLHttpRequest".equals(request.getHeader("X-Requested-With"));
+
+//        // session에 loginUser가 없을 경우
+//        if (loginUser == null) {
+//
+//            // 요청 처리를 중단
+//            if (isAjaxRequest) {
+//                // Handle AJAX request
+//                response.setContentType("application/json; charset=UTF-8");
+//                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401 Unauthorized
+//                PrintWriter out = response.getWriter();
+//                out.println("{\"message\":\"로그인이 필요한 페이지입니다.\"}");
+//                out.flush();
+//
+//            } else {
+//
+//                // 클라이언트에 alert 창을 띄우고, 메인 페이지로 리다이렉트
+//                response.setContentType("text/html; charset=UTF-8");
+//                PrintWriter out = response.getWriter();
+//                out.println("<script>alert('로그인이 필요한 페이지입니다.'); location.href='/';</script>");
+//                out.flush();    // 응답이 바로 전송되도록 버퍼 비우기
+//            }
+//            return false;
+//        }
 
         return true;
     }

d418386b847dcb7a91880b12640d19f9c7c8651e
chaSunil <cktjsdlf4636@naver.com>
2024-09-04 11:37:24 +0900

    feat: 상품 view 관련 Vo 가독성 좋게 주석추가
    상품 view 관련 Vo 가독성 좋게 주석추가

--- figurium/src/main/java/com/githrd/figurium/order/vo/MyOrderVo.java
+++ figurium/src/main/java/com/githrd/figurium/order/vo/MyOrderVo.java
@@ -11,29 +11,31 @@ import org.apache.ibatis.type.Alias;
 @Alias("myOrders")
 public class MyOrderVo {
 
-    private int id;
-    private String paymentType;
-    private int userId;
-    private String status;
-    private String createdAt;
+    private int id;             // 주문번호
+    private String paymentType; // 결제타입
+    private int userId;         // user번호
+    private String status;      // 주문상태
+    private String createdAt;   // 주문생성시간
 
-    private int price;
-    private int quantity;
-    private String name;
+    private int price;          // 주문상품가격
+    private int quantity;       // 주문상품갯수
+    private String name;        // 이름
 
-    private String customerPhone;
-    private String email;
-    private String recipientName;
-    private String phone;
+    private String customerPhone;   // 주문한 사람 번호
+    private String email;           // 주문한 사람 이메일
 
-    private String address;
-    private String deliveryRequest;
+    private String recipientName;   // 받는 사람 이름
+    private String phone;           // 받는 사람 번호
 
-    private String productName;
-    private String imageUrl;
-    private int productCount;
+    private String address;         // 받는 사람 주소
+    private String deliveryRequest; // 배송요청사항
 
-    private int totalValue;
+    private String productName;     // 상품이름
+    private String imageUrl;        // 상품이미지
+    private int productCount;       // 상품총갯수 - 1(외 1개, 외 2개 별도로 포함시키려는 컬럼)
+
+    // sum(price * quantity) as total_value  -- 각 행의 price * quantity의 총합
+    private int totalValue;         // order 번호에 대한 총가격 계산 컬럼
 
 
 }

90db4ec3d605447e78029cb8d7734bdff2319b2d
chaSunil <cktjsdlf4636@naver.com>
2024-09-06 09:48:32 +0900

    feat.깃 오류 수정 후 푸시
    깃 오류 수정 후 푸시

--- figurium/src/main/java/com/githrd/figurium/order/controller/PaymentController.java
+++ figurium/src/main/java/com/githrd/figurium/order/controller/PaymentController.java
@@ -12,8 +12,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -60,7 +58,7 @@ public class PaymentController {
         return api.paymentByImpUid(imp_uid);
     }
 
-    @GetMapping("/refund.do") // 민감한 결제처리(환불)이니 POST로 설정
+    @GetMapping("/refund.do")
     @ResponseBody
     public boolean requestRefund(Integer id, HttpServletResponse response) throws IOException {
 
